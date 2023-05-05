acf14b62dadd84deb1d7c9f273585f14ea3618bf
sourov-hossain1 <sourovhossain2122006@gmail.com>
2023-05-04 17:37:25 -0700

    some code added

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ app.get('/', (req, res) =>{
 app.get('/food', (req, res) =>{
     res.send(foodData);
 })
+app.get('/food/:id', (req, res) =>{
+    const id = req.params.id;
+    console.log(id);
+    const selectFood = foodData.find(n => n.id === id);
+    res.send(selectFood);
+})
 
 app.listen(port, () =>{
     console.log(`assignment ten is running on port: ${port}`)
